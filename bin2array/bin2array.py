fa5f79efc21b4e3fb300ef5cd01f25903c2dd8d4
Marcos Diaz <marcos.diaz@inputlabs.io>
2024-10-29 13:50:04 +0200

    Additional bin2array logging, template tweaks

--- bin2array/bin2array.py
+++ bin2array/bin2array.py
@@ -1,4 +1,5 @@
 def bin2string(path):
+    print(f'Converting {path}')
     array = open(path, 'rb').read()
     string = ''
     for i, byte in enumerate(array):
@@ -6,24 +7,30 @@ def bin2string(path):
         if not ((i+1) % 32):
             string += '\n    '
     string = string[:-1]
+    print(f'  {len(string)} bytes')
     return string
 
 
-template = open('bin2array.template', 'r', encoding='utf8').read()
+template = open('bin2array/template.c', 'r', encoding='utf8').read()
 
-firmware = bin2string('../build/llama.bin')
+firmware = bin2string('build/llama.bin')
 # bootloader = bin2string('../build/bootloader/bootloader.bin')
 # partition = bin2string('../build/partition_table/partition-table.bin')
 
-bin_file = template
-bin_file = template.replace('#firmware#', firmware)
-# bin_file = template.replace('#bootloader#', bootloader)
-# bin_file = template.replace('#partition#', partition)
+bin_file = template[:]
+bin_file = bin_file.replace('//firmware', firmware)
+bin_file = bin_file.replace('//bootloader', '0x00')
+bin_file = bin_file.replace('//partition', '0x00')
 
-empty_file = template
-empty_file = template.replace('#firmware#', '')
-# empty_file = template.replace('#bootloader#', '')
-# empty_file = template.replace('#partition#', '')
+empty_file = template[:]
+empty_file = empty_file.replace('//firmware', '0x00')
+empty_file = empty_file.replace('//bootloader', '0x00')
+empty_file = empty_file.replace('//partition', '0x00')
 
-open('../build/llama.bin.c', 'w', encoding='utf8').write(bin_file)
-open('../build/llama_empty.bin.c', 'w', encoding='utf8').write(empty_file)
+bin_file_path = 'build/llama.bin.c'
+print(f'Saving {bin_file_path} ({len(bin_file)} bytes)')
+open(bin_file_path, 'w', encoding='utf8').write(bin_file)
+
+empty_file_path = 'build/llama_empty.bin.c'
+print(f'Saving {empty_file_path} ({len(empty_file)} bytes)')
+open(empty_file_path, 'w', encoding='utf8').write(empty_file)
---
+++ bin2array/template.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <pico/platform.h>
+
+uint8_t __in_flash() ESP_FIRMWARE[] = {
+    //firmware
+};
+
+uint8_t __in_flash() ESP_BOOTLOADER[] = {
+    //bootloader
+};
+
+uint8_t __in_flash() ESP_PARTITION[] = {
+    //partition
+};
